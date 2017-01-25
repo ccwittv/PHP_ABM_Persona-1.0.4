7ebc8d757f48c65070e1bbcb0a4bb090fcf50c23
ccwittv <ccwittv@yahoo.com.ar>
2017-01-25 17:46:33 -0300

    -Se modifican algunos inputs utlizando atributos de bootstrap

---
+++ bd/clase1.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-01-2017 a las 21:40:27
+-- Versión del servidor: 5.6.24
+-- Versión de PHP: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `clase1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE IF NOT EXISTS `alumno` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `apellido` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `legajo` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `foto` varchar(50) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`id`, `nombre`, `apellido`, `legajo`, `foto`) VALUES
+(3, 'Ricardo', 'Maravilla', '67889-9', '67889-9.jpg'),
+(4, 'Homero J.', 'Simpson', '45609-8', '45609-8.jpg'),
+(5, 'Cristian', 'witt', '44303-6', '44303-6.jpg'),
+(6, 'Juanito', 'Efectivo', '440106-6', '440106-6.jpg');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<title>Ejemplos de ABM - con archivo de texto</title>
+	  
+		<?php require_once"partes/referencias.php" ;?>
+		<!--final de Estilos-->    
+
+      
+       
+</head>
+<body>
+	<?php		
+		require_once"partes/barraDeMenu.php";
+	 ?>
+	  <div class="container">
+
+	  	  <div class="page-header " >
+                <h1 class="animated bounceInRight">ABM - con archivo de texto Versión 1.0.2 Con foto</h1>      
+            </div>
+					<div class="CajaInicio animated bounceInRight">
+							<h1>ALUMNOS</h1>
+						
+							<form id="FormIngreso">
+ 										
+									    <a href="formAlta.php" class="list-group-item  list-group-item list-group-item-info">
+									      <h4 class="list-group-item-heading">Alta de Alumnos</h4>
+									    </a>
+										
+										<a href="formGrilla.php" class="list-group-item  list-group-item list-group-item-info">
+									      <h4 class="list-group-item-heading">Grilla de Alumnos</h4>
+									    </a>
+																											
+							</form>
+				   </div>
+		</div>
+</body>
+</html>
